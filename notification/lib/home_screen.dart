cad8b32d19811729d3905a55c65c9084e796f900
moheed25 <mohedaziz123@gmail.com>
2024-05-27 09:48:21 +0500

    update version commit

--- notification/lib/home_screen.dart
+++ notification/lib/home_screen.dart
@@ -36,71 +36,70 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('Flutter Notifications'),
       ),
-      body: Center(child: Text('Flutter Firebase Notifications')
-          //  TextButton(
-          //   onPressed: () async {
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            try {
+              final response = await http.post(
+                  Uri.parse('https://fcm.googleapis.com/fcm/send'),
+                  headers: <String, String>{
+                    'Content-Type': 'application/json; charset=UTF-8',
+                    'Authorization':
+                        'key=AAAAU9Lgttw:APA91bHYsa-K8NSyj_t7QYmNScsevaLFAY66I-AjnhePaTt9exBplGVDyvVCL0W22h1cZnBQ5b-BCnA1qzXrBSyl3OsTI0-PKNLTuGyci9mi_pEYkgSROHeiKqlQLcpdJS21Eu68SHr7',
+                  },
+                  body: jsonEncode(<String, dynamic>{
+                    'priority': 'high',
+                    'data': <String, dynamic>{
+                      'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+                      'status': 'done',
+                      "title": 'title',
+                      "body": 'body',
+                    },
+                    'notification': <String, dynamic>{
+                      "title": 'title',
+                      "body": 'body',
+                      "android_channel_id": "dbfood"
+                    },
+                    "to":
+                        'fUT-4rnQRGOCz14f1bvTAS:APA91bHvWt-bPfWhtJ0wcR5KdsM_8X53XnrIya4Gv0QeVGVQf04JoUapc7IHQ10H75brkU9tPpNVUMi4gxMrPJi23N3n78EadQkNy1haZG4i1oxpXg_GZwMOJ8ppU_uOCHlwvFSdrhTD',
+                  }));
+              print(response.body.toString());
+              print(response.statusCode.toString());
+            } catch (e) {
+              print(e);
+              if (kDebugMode) {
+                print("error push notifications");
+              }
+            }
 
-          // try {
-          //   final response = await http.post(
-          //       Uri.parse('https://fcm.googleapis.com/fcm/send'),
-          //       headers: <String, String>{
-          //         'Content-Type': 'application/json; charset=UTF-8',
-          //         'Authorization':
-          //             'key=AAAAU9Lgttw:APA91bHYsa-K8NSyj_t7QYmNScsevaLFAY66I-AjnhePaTt9exBplGVDyvVCL0W22h1cZnBQ5b-BCnA1qzXrBSyl3OsTI0-PKNLTuGyci9mi_pEYkgSROHeiKqlQLcpdJS21Eu68SHr7',
-          //       },
-          //       body: jsonEncode(<String, dynamic>{
-          //         'priority': 'high',
-          //         'data': <String, dynamic>{
-          //           'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-          //           'status': 'done',
-          //           "title": 'title',
-          //           "body": 'body',
-          //         },
-          //         'notification': <String, dynamic>{
-          //           "title": 'title',
-          //           "body": 'body',
-          //           "android_channel_id": "dbfood"
-          //         },
-          //         "to":
-          //             'fUT-4rnQRGOCz14f1bvTAS:APA91bHvWt-bPfWhtJ0wcR5KdsM_8X53XnrIya4Gv0QeVGVQf04JoUapc7IHQ10H75brkU9tPpNVUMi4gxMrPJi23N3n78EadQkNy1haZG4i1oxpXg_GZwMOJ8ppU_uOCHlwvFSdrhTD',
-          //       }));
-          //   print(response.body.toString());
-          //   print(response.statusCode.toString());
-          // } catch (e) {
-          //   print(e);
-          //   if (kDebugMode) {
-          //     print("error push notifications");
-          //   }
-          // }
-
-          // notificationServices.getDeviceToken().then((value) async {{
-          //   print(value.toString());
-          //   Map data =  {
-          //     "message":{
-          //       "token" :  value.toString(),
-          //       "priority": "high",
-          //       "mutable_content": true,
-          //       "notification":{
-          //         "title":"FCM Message",
-          //         "body":"This is an FCM notification message!",
-          //       }
-          //     }
-          //   };
-          //   var response = await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
-          //       body: json.encode(data),
-          //       headers: {
-          //         'Content-Type': 'application/json' ,
-          //         'Authorization': 'key=AAAAU9Lgttw:APA91bHYsa-K8NSyj_t7QYmNScsevaLFAY66I-AjnhePaTt9exBplGVDyvVCL0W22h1cZnBQ5b-BCnA1qzXrBSyl3OsTI0-PKNLTuGyci9mi_pEYkgSROHeiKqlQLcpdJS21Eu68SHr7'
-          //       }
-          //   );
-          //
-          //   print(response.statusCode.toString());
-          // }
-          // });
-          // },
-          // child: Text('Send'),
-
-          ),
+            // notificationServices.getDeviceToken().then((value) async {{
+            //   print(value.toString());
+            //   Map data =  {
+            //     "message":{
+            //       "token" :  value.toString(),
+            //       "priority": "high",
+            //       "mutable_content": true,
+            //       "notification":{
+            //         "title":"FCM Message",
+            //         "body":"This is an FCM notification message!",
+            //       }
+            //     }
+            //   };
+            //   var response = await http.post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
+            //       body: json.encode(data),
+            //       headers: {
+            //         'Content-Type': 'application/json' ,
+            //         'Authorization': 'key=AAAAU9Lgttw:APA91bHYsa-K8NSyj_t7QYmNScsevaLFAY66I-AjnhePaTt9exBplGVDyvVCL0W22h1cZnBQ5b-BCnA1qzXrBSyl3OsTI0-PKNLTuGyci9mi_pEYkgSROHeiKqlQLcpdJS21Eu68SHr7'
+            //       }
+            //   );
+            //
+            //   print(response.statusCode.toString());
+            // }
+            // });
+          },
+          child: Text('Send'),
+        ),
+      ),
     );
   }
 }
--- notification/lib/main.dart
+++ notification/lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:notification/firebase_options.dart';
 
 import 'package:notification/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
